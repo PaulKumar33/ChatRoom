952a37a0d8228d53fa51801a9cf9e9575784fc19
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2019-05-18 20:50:19 -0400

    For Dimitri

---
+++ initial_scripts/does_nothing.py
@@ -0,0 +1,14 @@
+
+class doesNothing:
+	def __init__(self):
+		print("this does nothing")
+	
+	def Well(self):
+		while(True):
+			print("Dimitri Sucks fat cock and Chris is still fat")
+	
+
+if __name__ == "__main__":
+	app = doesNothing()
+	app.Well()
+	
